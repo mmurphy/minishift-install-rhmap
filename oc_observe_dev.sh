2de2ee07075e060a548034089b70cd1c85273e43
David Ffrench <davidffrench@gmail.com>
2018-07-13 14:04:23 +0100

    Adding docker_pull_secret to deployer service account

--- oc_observe_dev.sh
+++ oc_observe_dev.sh
@@ -3,4 +3,5 @@ oc secrets new docker-pull-secret .dockerconfigjson=${HOME}/.docker/config.json 
 if [ $? -eq 0 ]; then
     echo "Created secret in namespace $1" && sleep 3
     oc secrets link default docker-pull-secret --for=pull --namespace=$1
+    oc secrets link deployer docker-pull-secret --for=pull --namespace=$1
 fi
